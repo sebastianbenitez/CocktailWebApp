10785147e263826961f60d88d054c9cd781e83c4
sebastianbenitez <58483040+sebastianbenitez@users.noreply.github.com>
2020-03-05 02:00:08 -0300

    Corregido tema de request
    Arreglado problema de scopes con los request, solucion no satisfactoria: tener que anidar requests

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-var request = require("request");
 
+var request = require("request");
 var options = {
   method: 'GET',
-  url: 'https://the-cocktail-db.p.rapidapi.com/random.php',
   headers: {
     'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com',
     'x-rapidapi-key': '4ef122cb3bmsh2d31ab02b6fef2ap18000djsn1979327c68c3'
   }
 };
 
-var randomCocktail;
-
-// obtengo cocktail al azar
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
-	
-	var parsed = JSON.parse(body);
-	randomCocktail = parsed.drinks[0];
-});
-
-// mediante el id del cocktail al azar, obtengo la informacion restante
-options.url = 'https://the-cocktail-db.p.rapidapi.com/lookup.php';
-options.qs= {
-	i: randomCocktail.idDrink
-};
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
-	
-	var parsed = JSON.parse(body);
-	randomCocktail = parsed.drinks[0];
-});
-
 
 router.get('/', function(req, res, next) {
-	res.render('index', { cocktail: randomCocktail });
+
+	// obtengo cocktail al azar
+	options.url = 'https://the-cocktail-db.p.rapidapi.com/random.php';
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error);
+		
+		var parsed = JSON.parse(body);
+		var randomCocktail = parsed.drinks[0];
+
+		// mediante el id del cocktail al azar, obtengo la informacion restante
+		options.url = 'https://the-cocktail-db.p.rapidapi.com/lookup.php';
+		options.qs= {
+			i: randomCocktail.idDrink
+		};
+
+		request(options, function (error, response, body) {
+			if (error) throw new Error(error);
+			
+			var parsed = JSON.parse(body);
+			var randomCocktail = parsed.drinks[0];
+			res.render('index', { cocktail: randomCocktail });
+		});
+	});
 });
 
 
